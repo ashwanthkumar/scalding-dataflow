03cf9e171867ac534c517c6686ec9a5189e351f5
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-03 14:54:05 +0530

    Adding name to SContext

--- src/main/scala/in/ashwanthkumar/scaldingdataflow/SContext.scala
+++ src/main/scala/in/ashwanthkumar/scaldingdataflow/SContext.scala
@@ -5,15 +5,15 @@ import cascading.pipe.Pipe
 import com.twitter.scalding.{Hdfs, Local, Mode, Test}
 import org.apache.hadoop.conf.Configuration
 
-case class SContext(pipe: Pipe, flowDef: FlowDef, mode: Mode) {
+case class SContext(pipe: Pipe, flowDef: FlowDef, mode: Mode, name: String) {
   def apply(transformed: Pipe) = this.copy(pipe = transformed)
 }
 
 object SContext {
-  def local(name: String) = SContext(new Pipe(name), FlowDef.flowDef(), Local(false))
+  def local(name: String) = SContext(new Pipe(name), FlowDef.flowDef(), Local(false), name)
   def test(name: String) = SContext(new Pipe(name), FlowDef.flowDef(),
     // FIXME - Need to build wrapeers like in JobTest, btw this is horrible :(
-    Test({ _ => None }))
+    Test({ _ => None }), name)
 
-  def hdfs(name: String) = SContext(new Pipe(name), FlowDef.flowDef(), Hdfs(strict = false, new Configuration))
+  def hdfs(name: String) = SContext(new Pipe(name), FlowDef.flowDef(), Hdfs(strict = false, new Configuration), name)
 }
