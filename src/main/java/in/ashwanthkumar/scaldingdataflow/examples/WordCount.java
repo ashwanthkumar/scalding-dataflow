5448336cf8671b2cc54cd2ec7c9885f3e9ea1420
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-05 00:06:44 +0530

    Moving WordCount from Scala to Java
    Writing Java in Scala is so ugly :(

---
+++ src/main/java/in/ashwanthkumar/scaldingdataflow/examples/WordCount.java
@@ -0,0 +1,33 @@
+package in.ashwanthkumar.scaldingdataflow.examples;
+
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.options.PipelineOptions;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+import com.google.cloud.dataflow.sdk.transforms.Count;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.values.KV;
+
+public class WordCount {
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory
+                .fromArgs(args)
+                .withValidation()
+                .create();
+        Pipeline pipeline = Pipeline.create(options);
+
+        pipeline.apply(TextIO.Read.from("kinglear.txt").named("Source"))
+            .apply(Count.<String>perElement())
+            .apply(ParDo.of(new DoFn<KV<String, Long>, String>() {
+                @Override
+                public void processElement(ProcessContext c) throws Exception {
+                    KV<String, Long> kv = c.element();
+                    c.output(String.format("%s\t%d", kv.getKey(), kv.getValue()));
+                }
+            }))
+            .apply(TextIO.Write.to("out.txt").named("Sink"));
+
+        pipeline.run();
+    }
+}
