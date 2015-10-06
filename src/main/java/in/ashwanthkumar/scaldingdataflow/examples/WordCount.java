e4fa6c83e619b5c46aa521939b792f1fb8c4709e
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-06 19:55:49 +0530

    Creating custom Options for WordCount

--- src/main/java/in/ashwanthkumar/scaldingdataflow/examples/WordCount.java
+++ src/main/java/in/ashwanthkumar/scaldingdataflow/examples/WordCount.java
@@ -2,22 +2,38 @@ package in.ashwanthkumar.scaldingdataflow.examples;
 
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.options.Default;
+import com.google.cloud.dataflow.sdk.options.Description;
 import com.google.cloud.dataflow.sdk.options.PipelineOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
 import com.google.cloud.dataflow.sdk.transforms.Count;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
 import com.google.cloud.dataflow.sdk.values.KV;
+import in.ashwanthkumar.scaldingdataflow.ScaldingPipelineOptions;
 
 public class WordCount {
+    interface Options extends ScaldingPipelineOptions {
+        @Description("Input file")
+        @Default.String("kinglear.txt")
+        String getInput();
+        void setInput(String input);
+
+        @Description("Output file")
+        @Default.String("out.txt")
+        String getOutput();
+        void setOutput(String output);
+    }
+
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory
+        Options options = PipelineOptionsFactory
                 .fromArgs(args)
                 .withValidation()
-                .create();
+                .create()
+                .as(Options.class);
         Pipeline pipeline = Pipeline.create(options);
 
-        pipeline.apply(TextIO.Read.from("kinglear.txt").named("Source"))
+        pipeline.apply(TextIO.Read.from(options.getInput()).named("Source"))
             .apply(ParDo.named("splitter").of(new DoFn<String, String>() {
                 @Override
                 public void processElement(ProcessContext c) throws Exception {
@@ -37,7 +53,7 @@ public class WordCount {
                     c.output(String.format("%s\t%d", kv.getKey(), kv.getValue()));
                 }
             }))
-            .apply(TextIO.Write.to("out.txt").named("Sink"));
+            .apply(TextIO.Write.to(options.getOutput()).named("Sink"));
 
         pipeline.run();
     }
